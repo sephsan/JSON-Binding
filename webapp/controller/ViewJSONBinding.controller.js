8864fb0ba188002dead8b6d30719a5860e292d0a
Joseph Bernell Agbing <satsvn_josephba@sats.com.sg>
2025-11-04 00:37:20 +0000

    working with UI

--- webapp/controller/ViewJSONBinding.controller.js
+++ webapp/controller/ViewJSONBinding.controller.js
@@ -1,10 +1,35 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("sapips.training.jsonbinding.controller.ViewJSONBinding", {
         onInit() {
+            var mydata = {
+                            "Eid":"1",
+                            "Enabled":true,
+                            Address:{
+                                    "Street":"Santan",
+                                    "City":"Sto Tomas",
+                                    "Zip":"122",
+                                    "Country":"Ph"
+                                    },
+                            "SalesAmount":"88",
+                            "CurrencyCode":"" 
+                          };
+           var oModel_local = new JSONModel();
+           oModel_local.setData(mydata);
+           this.getView().setModel(oModel_local);
+        },
+        formatAddress:function()
+        {
+            var oModel  = this.getView().getModel();
+            var address = oModel.getProperty("/Address").Street;
+            var zip     = oModel.getProperty("/Address").Zip;
+            var city    = oModel.getProperty("/Address").City;
+            var country = oModel.getProperty("/Address").Country;
+            return `${address } ${zip}  ${city } ${country}`; 
         }
     });
 });
